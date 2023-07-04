37ca2e8320b71f574e42f8cf8c22514f5f48caa0
DaoQuocViet18 <108569142+DaoQuocViet18@users.noreply.github.com>
2023-07-04 17:44:54 +0700

    time_delay_to_stop

--- Tetris/Assets/Scripts/Game_Scripts/Border.cs
+++ Tetris/Assets/Scripts/Game_Scripts/Border.cs
@@ -1,32 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class Border : MonoBehaviour
 {
     private Moving_block moving_Block;
+    [Header("Down")]
     public float raycastDistance = 1f;
-    public LayerMask layerMask;
-    // Start is called before the first frame update
+    public LayerMask layerMask_Stop;
+
+    [Header("Left Right")]
+    public float raycastDistance_left_right = 1f;
+
     void Start()
     {
         moving_Block = GetComponent<Moving_block>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void time_delay_to_stop()
     {
-        if (Physics2D.Raycast(transform.position, Vector2.down, raycastDistance, layerMask) && !moving_Block.stop)
-        {
-            moving_Block.stop = true;
-            foreach (GameObject jun in moving_Block.block_junior)
-                jun.gameObject.layer = 3;
-            moving_Block.spawn_modosl();
-        }
-        Debug.DrawRay(transform.position, Vector2.down * raycastDistance, Color.red);
-    }
+        if (!moving_Block.stop)
+            foreach (GameObject block in moving_Block.block_junior)
+                if (Physics2D.Raycast(block.transform.position, Vector2.down, raycastDistance, layerMask_Stop) && !moving_Block.stop)
+                {
+                    moving_Block.stop = true;
+                    foreach (GameObject jun in moving_Block.block_junior)
+                        jun.gameObject.layer = 3;
+                    moving_Block.Invoke("spawn", 1);
+                    break;
+                }
 
-    public void check_block ()
+        if (!moving_Block.stop)
+            moving_Block.move_Down_Slow();   
+    }    
+
+    public void check_border_block ()
     {
         foreach (GameObject block in moving_Block.block_junior)
         {
@@ -34,6 +43,30 @@ public class Border : MonoBehaviour
                 transform.Translate(Vector2.right * moving_Block.speed_move, Space.World);
             else if (block.transform.position.x >= 2)
                 transform.Translate(Vector2.left * moving_Block.speed_move, Space.World);
-        }    
+        }
+    }
+
+    public bool check_block_Down()
+    {
+        foreach (GameObject block in moving_Block.block_junior)
+            if (Physics2D.Raycast(block.transform.position, Vector2.down, raycastDistance * 3, layerMask_Stop))
+                return true;
+
+        return false;
+    }
+
+    public bool check_block_L()
+    {
+        foreach (GameObject block in moving_Block.block_junior)
+            if (Physics2D.Raycast(block.transform.position, Vector2.left, raycastDistance_left_right, layerMask_Stop))
+                return false;
+        return true;
+    }
+    public bool check_block_R()
+    {
+        foreach (GameObject block in moving_Block.block_junior)
+            if (Physics2D.Raycast(block.transform.position, Vector2.right, raycastDistance_left_right, layerMask_Stop))
+                return false;
+        return true;
     }    
 }
--- Tetris/Assets/Scripts/Game_Scripts/Moving_block.cs
+++ Tetris/Assets/Scripts/Game_Scripts/Moving_block.cs
@@ -20,21 +20,27 @@ public class Moving_block : MonoBehaviour
     [Header("Stop")]
     public bool stop = false;
     public GameObject[] block_junior;
+    public float time_to_Stop = 0.5f;
     private Border border;
     private Spawn_block spawn_Block;
     private Display_blocks display_Blocks;
-    void Start()
+
+    private void Awake()
     {
         Transform[] childTransforms = GetComponentsInChildren<Transform>();
         block_junior = new GameObject[childTransforms.Length - 1];
         for (int i = 1; i < childTransforms.Length; i++)
         {
             block_junior[i-1] = childTransforms[i].gameObject;
-        }
+        }      
+    }
+    void Start()
+    {
         border = GetComponent<Border>();
         spawn_Block = GameObject.Find("Spawn_block").GetComponent <Spawn_block>();
         display_Blocks = GameObject.Find("Display_board").GetComponent<Display_blocks>();
-        InvokeRepeating("move_Down_Slow", 0.5f, 1);
+        move_Down_Slow();
+        //InvokeRepeating("move_Down_Slow", 0.5f, 1);
     }
 
     void Update()
@@ -43,44 +49,41 @@ public class Moving_block : MonoBehaviour
             move();
     }
 
-    public  void spawn_modosl()
-    {
-        Invoke("spawn", 1);
-        CancelInvoke("move_Down_Slow");
-    }    
-
-    private void spawn()
+    public void spawn()
     {
             spawn_Block.SpawnObject();
             display_Blocks.ActivateRandomObject();    
-    }    
-    private void move_Down_Slow()
+    }
+
+    public void move_Down_Slow()
     {
         transform.Translate(Vector2.down * speed_Down, Space.World);
+        border.Invoke("time_delay_to_stop", time_to_Stop);
     }    
 
     private void move()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             transform.Rotate(0, 0, transform.position.z + 90f);
-            border.check_block();
+            border.check_border_block();
         }    
 
-        if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > -3.5f)
+        if (Input.GetKeyDown(KeyCode.DownArrow) && border.check_block_Down() == false)
             transform.Translate(Vector2.down * speed_Down, Space.World); 
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > -6)
-        { 
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && border.check_block_L() == true)
+        {
             transform.Translate(Vector2.left * speed_move, Space.World);
-            border.check_block();
+            border.check_border_block();
         }    
-        else if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < 2)
-        {     
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && border.check_block_R() == true)
+        {   
             transform.Translate(Vector2.right * speed_move, Space.World);
-            border.check_block();
+            border.check_border_block();
         }
-    transform.position = new Vector2(Mathf.Floor(transform.position.x / 0.1f) * 0.1f, transform.position.y);
+
+        transform.position = new Vector2(Mathf.Floor(transform.position.x / 0.1f) * 0.1f, transform.position.y);
     }
 
 }
